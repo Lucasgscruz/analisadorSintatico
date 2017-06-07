382447f1d52dfcc0776625c00c3e4985d188fe6a
Lucas <lucasgscruz10@hotmail.com>
2017-06-06 22:37:02 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Analisador Sintático
+Projeto apresentado ao curso de Ciência da Computação, da
+Universidade Federal de São João del Rei, como requisito parcial 
+para obtenção da nota final da disciplina de Compiladores.
+
+## Executando
+
+
+### Desenvolvido por:
+![](https://github.com/Lucasgscruz.png?size=100)
+Lucas Cruz ([github](https://github.com/lucasgscruz))
---
+++ src/dicionario.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+if __name__ != '__main__':
+
+    delimitadores = [' ', '(', ')', '[', ']', '{', '}', ';', ',', "'", '"']
+    aritmeticos = ["%", "+", "-", "/", "*", "^"]
+    logicos = ['>', '<', '!', '&', '=', '|']
+    uniaoLogicos = ['>=', '<=', '==', '&&', '||', '!=']
+    reservadas = ['if', 'else', 'break', 'while',
+                  'print', 'char', 'int', 'float','for', 'return']
